3670345c9ba3fb19db20333c88430aafe64ae549
Gary <garyzhu1996@126.com>
2023-04-03 04:32:07 +0800

    Update test_only.py

--- core/test_only.py
+++ core/test_only.py
@@ -17,25 +17,9 @@ import utils.helpers
 import core.pipeline_test as pipeline
 
 from models.encoder import Encoder
-
-# from models.decoder import Decoder
 from models.decoder_pre_merger import Decoder
-# from models.decoder_pre_merger_ebam_64 import Decoder
-# from models.decoder_pre_merger_sa import Decoder
-# from models.decoder_multi_merger_5fusion import Decoder
-
-# from models.refiner import Refiner
 from models.refiner_3dresunet import Refiner
-# from models.refiner_resunet_64 import Refiner
-
-# from models.merger import Merger
 from models.merger_pre_merger import Merger
-# from models.merger_concat_res_pre_merger_64 import Merger
-# from models.merger_concat_pre_merger import Merger
-# from models.merger_concat_res_pre_merger_with_supervise import Merger
-# from models.merger_concat_res_multi_level_fusion import Merger
-
-from sklearn.decomposition import PCA
 
 
 def test_net(cfg):
@@ -55,14 +39,6 @@ def test_net(cfg):
     
     if cfg.CONST.IMB_WEIGHTS is not None:
         mlp = pipeline.setup_mlp(cfg)
-    elif cfg.CONST.DIMENSION_AFTER_PCA_POOLING_ADD is not None:
-        estimator = PCA(n_components=cfg.CONST.DIMENSION_AFTER_PCA_POOLING_ADD)
-        # avg_pool = torch.nn.AdaptiveAvgPool2d(7)
-        # max_pool = torch.nn.AdaptiveMaxPool2d(7)
-    elif cfg.CONST.DIMENSION_AFTER_PCA_POOLING_CONCAT is not None:
-        estimator = PCA(n_components=cfg.CONST.DIMENSION_AFTER_PCA_POOLING_CONCAT)
-        avg_pool = torch.nn.AdaptiveAvgPool2d(7)
-        max_pool = torch.nn.AdaptiveMaxPool2d(7)
 
     # Testing loop
     n_samples = len(test_data_loader)
@@ -86,58 +62,24 @@ def test_net(cfg):
             ground_truth_volume = utils.helpers.var_or_cuda(ground_truth_volume)
 
             # Test the encoder, decoder, refiner and merger
-            # image_features = encoder(rendering_images)
             image_features = encoder.module.resnet_forward(rendering_images).unsqueeze(dim=0)
 
             if cfg.CONST.VIEWS_AFTER_REDUCTION is not None:
                 if cfg.CONST.IMB_WEIGHTS is not None:
-                    # mapping_features = mlp(image_features.view(image_features.shape[1], 256, 7, 7))
-                    # mapping_features = mlp(image_features.view(image_features.shape[1], 512, 28, 28))
                     mapping_features = mlp(image_features.view(image_features.shape[1], 256, 56, 56))
-                elif cfg.CONST.DIMENSION_AFTER_PCA_POOLING_ADD is not None:
-                    # mapping_features = (avg_pool(image_features.view(image_features.shape[1], 256, 56, 56)) +
-                    #                     max_pool(image_features.view(image_features.shape[1], 256, 56, 56))).view(-1, 7 * 7 * 256)
-                    # mapping_features = \
-                    #     torch.tensor(estimator.fit_transform(image_features.cpu())).cuda()
-                    mapping_features = torch.tensor(estimator.fit_transform(
-                        image_features.view(image_features.shape[1], 256 * 56 * 56).cpu())).cuda()
-                elif cfg.CONST.DIMENSION_AFTER_PCA_POOLING_CONCAT is not None:
-                    mapping_features = torch.cat([avg_pool(image_features.view(image_features.shape[1], 256, 56, 56)),
-                                                  max_pool(image_features.view(image_features.shape[1], 256, 56, 56))],
-                                                 dim=1).view(-1, 2 * 7 * 7 * 256)
-                    mapping_features = \
-                        torch.tensor(estimator.fit_transform(mapping_features.cpu())).cuda()
-                else:
-                    # mapping_features = image_features.view(image_features.shape[1], 256 * 7 * 7)
-                    mapping_features = image_features.view(image_features.shape[1], 256 * 56 * 56)
-                # views = pipeline.reduce_branch_kmeans(mapping_features, cfg.CONST.VIEWS_AFTER_REDUCTION)
                 views = pipeline.reduce_branch_fps(mapping_features, cfg.CONST.VIEWS_AFTER_REDUCTION)
                 image_features = image_features[:, views, ::]
 
             image_features = encoder.module.other_layers_forward(image_features.squeeze(dim=0), image_features.shape[1])
-
-            if not cfg.NETWORK.BI_MERGER:
-                raw_features, generated_volume = decoder(image_features)
-            else:
-                raw_features, generated_volume, fusion_features = decoder(image_features)
+            raw_features, generated_volume = decoder(image_features)
 
             if cfg.NETWORK.USE_MERGER:
-                if cfg.TRAIN.MULTI_LEVEL_MEAN_SUPERVISE:
-                    generated_volume, fusion_volumes = merger(raw_features, generated_volume)
-                elif cfg.TRAIN.MULTI_LEVEL_WEIGHTED_SUPERVISE:
-                    generated_volume, fusion_volumes = merger(raw_features, generated_volume, True)
-                elif cfg.NETWORK.BI_MERGER:
-                    generated_volume = merger(raw_features, generated_volume, fusion_features)
-                else:
-                    generated_volume = merger(raw_features, generated_volume)
+                generated_volume = merger(raw_features, generated_volume)
             else:
                 generated_volume = torch.mean(generated_volume, dim=1)
 
             if cfg.NETWORK.USE_REFINER:
-                if cfg.NETWORK.BI_MERGER and generated_volume.shape[0] == 1:
-                    generated_volume = refiner(generated_volume.unsqueeze(dim=1).expand(-1, 2, -1, -1, -1))
-                else:
-                    generated_volume = refiner(generated_volume)
+                generated_volume = refiner(generated_volume)
 
             # IoU per sample
             sample_iou = []
